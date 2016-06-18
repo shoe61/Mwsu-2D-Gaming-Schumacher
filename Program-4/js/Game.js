bffea7b7880d07020e9022c00d55754429249560
Christian Norfleet <cynorfleet@gmail.com>
2016-06-18 16:11:02 -0500

    Revert "Revert "ADDED MY CODE FOR PUSHING OBJECTS""

--- Program-4/js/Game.js
+++ Program-4/js/Game.js
@@ -7,46 +7,17 @@ var Easy = 2550;
 var Medium = 50150;
 var Hard = 150250;
 // The ratio of large asteroids (0-100)
-var asteroidSize
-
-
-
-//next five lines necessary for ship movement and bullets:
-var sprite;
-var cursors;
-var bullet;
-var bullets;
-var bulletTime = 0;
-//endo ship and bullets stuff
-
-
+var astroidarray = [];
+var ARRAY_NUM_TOTAL = 1000;
 
 //title screen
 SpaceHipster.Game = function(){};
 
 SpaceHipster.Game.prototype = {
-     
   create: function() {
+
   	//set world dimensions
     this.game.world.setBounds(0, 0, 1920, 1920);
-      
-    //added for ship and bullet
-    //  We need arcade physics
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    //  This will run in Canvas mode, so let's gain a little speed and display
-    this.game.renderer.clearBeforeRender = false;
-    this.game.renderer.roundPixels = true;
-    //  Our ships bullets
-    bullets = this.game.add.group();
-    bullets.enableBody = true;
-    bullets.physicsBodyType = Phaser.Physics.ARCADE;
-    //  All 40 of them
-    bullets.createMultiple(40, 'bullet');
-    bullets.setAll('anchor.x', 0.5);
-    bullets.setAll('anchor.y', 0.5);
-    //endo ship and bullet stuff
-      
-    this.game.time.events.loop(0500, this.generateAsteroid, this);
 
     //background
     this.background = this.game.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'space');
@@ -54,40 +25,27 @@ SpaceHipster.Game.prototype = {
     //create player
     this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'playership');
     this.player.scale.setTo(2);
-    //enable player physics
-    this.game.physics.arcade.enable(this.player);
-    this.player.body.drag.set(025);
-    this.player.body.maxVelocity.set(500);
-      
-      
-    // this.playerSpeed = 120; (replaced by ship and bullet stuff)
-    this.player.body.collideWorldBounds = true; 
-      
-      
-      
-    /*these lines are not needed- our sprite doesn't fly
     this.player.animations.add('fly', [0, 1, 2, 3], 5, true);
-    this.player.animations.play('fly');*/
-      
-    this.asteroids = this.game.add.group();
-    //enable physics in them
-    this.asteroids.enableBody = true;
-      
-    var asteroid; //this line can be in or out...no collision.... 
-      
-      
+    this.player.animations.play('fly');
+
+      //initializing the physics of asteroids
+      this.asteroids = this.game.add.group();
+      //enable physics in them
+      this.asteroids.enableBody = true;
 
     //player initial score of zero
     this.playerScore = 0;
 
-    
+    //enable player physics
+    this.game.physics.arcade.enable(this.player);
+    this.playerSpeed = 120;
+    this.player.body.collideWorldBounds = true;
 
     //the camera will follow the player in the world
     this.game.camera.follow(this.player);
 
     //generate game elements
     this.generateCollectables();
-        
 
     //show score
     this.showLabels();
@@ -96,6 +54,15 @@ SpaceHipster.Game.prototype = {
     this.explosionSound = this.game.add.audio('explosion');
     console.log(this.explosionSound);
     this.collectSound = this.game.add.audio('collect');
+
+    //Populate astroid sizes and store size
+    this.sizeGen();
+
+    //Debug
+    console.log(astroidarray)
+
+    // every 10 secs process generateAsteroid
+    this.game.time.events.loop(500, this.generateAsteriod, this);
   },
   update: function() {
     if(this.game.input.activePointer.justPressed()) {
@@ -110,6 +77,56 @@ SpaceHipster.Game.prototype = {
     //overlapping between player and collectables
     this.game.physics.arcade.overlap(this.player, this.collectables, this.collect, null, this);
   },
+
+    sizeGen: function(){
+        //generate the ratio of large rocks
+        var rock_size_percent = this.game.rnd.integerInRange(0, 100);
+        //DEBUG
+        console.log("rocksize%: " + rock_size_percent);
+        //if zero percent of rocks are large
+        if (rock_size_percent == 0)
+            //push all small rocks to array
+            for(var x = 0; x < ARRAY_NUM_TOTAL; x++)
+                this.pushRocksToArray("small");
+        //if 100 percent of rocks are large
+        else if (rock_size_percent == 100)
+            //push all large rocks to array
+            for(var x = 0; x < ARRAY_NUM_TOTAL; x++)
+                this.pushRocksToArray("large");
+        //if mixed ratio of rock sizes
+        else{
+            //push the percent of remaining small rocks to array
+            for(var x = 0; x < Math.round(ARRAY_NUM_TOTAL * ( 1 - rock_size_percent/100)); x++)
+                this.pushRocksToArray("small");
+            //push the percent of large rocks to array
+            for(var x = 0; x < Math.round(ARRAY_NUM_TOTAL * (rock_size_percent/100)); x++)
+                this.pushRocksToArray("large");
+        }
+    },
+
+    pushRocksToArray: function(size){
+        var rockType = {
+    sizePick: null,
+    sizeType: ""
+            };
+        //if wanting to push big rock object to array
+        if (size == "large"){
+            //puts random large size in rock object
+            rockType.sizePick = this.game.rnd.integerInRange(90, 128);
+            //tags rock as being large
+            rockType.sizeType = "large";
+            //pushes it to the array of astroids
+            astroidarray.push(rockType);
+        }
+        //if wanting to push small rock object to array
+        else{
+            rockType.sizePick = this.game.rnd.integerInRange(16, 32);
+            rockType.sizeType = "small"
+            astroidarray.push(rockType);
+        }
+        console.log(rockType);
+    },
+
   generateCollectables: function() {
     this.collectables = this.game.add.group();
 
@@ -118,7 +135,7 @@ SpaceHipster.Game.prototype = {
     this.collectables.physicsBodyType = Phaser.Physics.ARCADE;
 
     //phaser's random number generator
-    var numCollectables = this.game.rnd.integerInRange(100, 150)
+    var numCollectables = this.game.rnd.integerInRange(100, 150);
     var collectable;
 
     for (var i = 0; i < numCollectables; i++) {
@@ -129,19 +146,29 @@ SpaceHipster.Game.prototype = {
     }
 
   },
- 
-    generateAsteroid: function() {
-      //add sprite
-      asteroid = this.asteroids.create(this.game.world.randomX, this.game.world.randomY, 'rock');
-      asteroid.scale.setTo(this.game.rnd.integerInRange(10, 40)/10);
+
+    generateAsteriod: function(size) {
+        var asteriod;
+        //random large size ratio generator(0-100)
+        var sizeseed = this.game.rnd.integerInRange(0-100);
+        if(sizeseed == 0)
+            astroidarray = this.game.rnd.integerInRange(16, 47);
+
+      // MAKE THE ASTEROID
+      asteriod = this.asteroids.create(this.game.world.randomX, this.game.world.randomY, 'rock');
+        //scale asteroid by picking from arrayindex and grabbing its size data
+        var pik = this.game.rnd.weightedPick(astroidarray).sizePick / 1000 * 20;
+      asteriod.scale.setTo(pik);
 
       //physics properties
-      asteroid.body.velocity.x = this.game.rnd.integerInRange(-20, 20);
-      asteroid.body.velocity.y = this.game.rnd.integerInRange(-20, 20);
-      asteroid.body.immovable = true;
-      //this.game.physics.arcade.enable(asteroid); that didn't fix it.
-      asteroid.body.collideWorldBounds = true;
- },
+      asteriod.body.velocity.x = this.game.rnd.integerInRange(-20, 20);
+      asteriod.body.velocity.y = this.game.rnd.integerInRange(-20, 20);
+      asteriod.body.immovable = true;
+      asteriod.body.collideWorldBounds = true;
+
+        //DEBUG
+        console.log(asteriod.height)
+      },
 
   hitAsteroid: function(player, asteroid) {
     //play explosion sound
@@ -186,5 +213,5 @@ SpaceHipster.Game.prototype = {
 TODO
 
 -audio
--asteroid bounch
+-asteriod bounch
 */
